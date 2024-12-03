fd6670757ef42eed0fcbf12ae2c4c2aeb090bb9e
ebbam <99182071+ebbam@users.noreply.github.com>
2024-12-03 01:21:27 +0000

    Created cleaning script and consolidated entsoe dataset

---
+++ data/raw/ENTSOE_load_data/cleaning_entsoe_load.R
@@ -0,0 +1,59 @@
+#### CLEANING ENTSO-E Load Data ##
+# https://www.entsoe.eu/data/power-stats/ - "Monthly Hourly Load Values"
+
+library(here)
+library(tidyverse)
+library(readxl)
+library(assertthat)
+library(janitor)
+library(lubridate)
+
+full_df <- tibble()
+
+#### 2019-2024
+for(yr in 2019:2024){
+  temp <- read_xlsx(here(paste0("data/raw/ENTSOE_load_data/monthly_hourly_load_values_", yr, ".xlsx")))
+  
+  assert_that(n_distinct(temp$MeasureItem) == 1)
+  assert_that(n_distinct(temp$Cov_ratio) == 1)
+  
+  full_df <- temp %>% 
+    select(DateUTC, CountryCode, Value_ScaleTo100, Cov_ratio) %>% 
+    clean_names %>% 
+    rbind(full_df, .)
+  
+}
+
+#### 2015-2019
+### NOTE THAT COVERAGE RATIO IS NOT 100 for all observations in this (6% of observations ahve 98covratio) - Solved: ScaleTo100 corrects this!
+temp <- read_xlsx(here("data/raw/ENTSOE_load_data/MHLV_data-2015-2019.xlsx"))
+
+assert_that(n_distinct(temp$MeasureItem) == 1)
+
+full_df <- temp %>% 
+  select(DateUTC, CountryCode, Value_ScaleTo100, Cov_ratio) %>% 
+  clean_names %>% 
+  rbind(full_df, .)
+
+
+#### 2006-2015
+full_df <- read_xlsx(here("data/raw/ENTSOE_load_data/Monthly-hourly-load-values_2006-2015.xlsx"), skip = 3) %>% 
+  pivot_longer(!c(Country, Year, Month, Day, `Coverage ratio`), names_to = "Hour") %>% 
+  rename(cov_ratio = `Coverage ratio`, 
+         country_code = Country) %>% 
+  mutate(value_scale_to100 = value/(cov_ratio/100),
+         date_utc = ymd_h(paste(Year, Month, Day, Hour, sep = "-"))) %>% 
+  clean_names %>% 
+  select(date_utc, country_code, value_scale_to100, cov_ratio) %>% 
+  rbind(full_df, .) %>% 
+  arrange(date_utc, country_code)
+
+
+full_df %>% 
+  ggplot() +
+  geom_line(aes(x = date_utc, y = value_scale_to100)) +
+  facet_wrap(~country_code, scales = "free")
+
+full_df %>% 
+  rename(load = value_scale_to100) %>% 
+  saveRDS(here("data/out/entsoe_load_data_consolidated.rds"))
