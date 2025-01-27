8d45e2ca7ce780bd6cf926dbc6452844a8ed4577
ebbam <99182071+ebbam@users.noreply.github.com>
2025-01-27 11:07:07 +0000

    ENTSOE load data moved to newly created parent ENTSOE file

--- data/raw/ENTSOE/ENTSOE_load_data/cleaning_entsoe_load.R
+++ data/raw/ENTSOE/ENTSOE_load_data/cleaning_entsoe_load.R
@@ -12,7 +12,7 @@ full_df <- tibble()
 
 #### 2019-2024
 for(yr in 2019:2024){
-  temp <- read_xlsx(here(paste0("data/raw/ENTSOE_load_data/monthly_hourly_load_values_", yr, ".xlsx")))
+  temp <- read_xlsx(here(paste0("data/raw/ENTSOE/ENTSOE_load_data/monthly_hourly_load_values_", yr, ".xlsx")))
   
   assert_that(n_distinct(temp$MeasureItem) == 1)
   assert_that(n_distinct(temp$Cov_ratio) == 1)
@@ -27,7 +27,7 @@ for(yr in 2019:2024){
 #### 2015-2018
 ### NOTE THAT COVERAGE RATIO IS NOT 100 for all observations in this (6% of observations have 98covratio) - Solved: ScaleTo100 corrects this!
 # Sheet one includes data from 2015-2017
-temp <- read_xlsx(here("data/raw/ENTSOE_load_data/MHLV_data-2015-2019.xlsx"), sheet = 1)
+temp <- read_xlsx(here("data/raw/ENTSOE/ENTSOE_load_data/MHLV_data-2015-2019.xlsx"), sheet = 1)
 
 assert_that(n_distinct(temp$MeasureItem) == 1)
 
@@ -37,7 +37,7 @@ full_df <- temp %>%
   rbind(full_df, .)
 
 # Sheet 2 includes data from 2018
-temp <- read_xlsx(here("data/raw/ENTSOE_load_data/MHLV_data-2015-2019.xlsx"), sheet = 2)
+temp <- read_xlsx(here("data/raw/ENTSOE/ENTSOE_load_data/MHLV_data-2015-2019.xlsx"), sheet = 2)
 
 assert_that(n_distinct(temp$MeasureItem) == 1)
 
@@ -48,7 +48,7 @@ full_df <- temp %>%
 
 
 #### 2006-2015
-full_df <- read_xlsx(here("data/raw/ENTSOE_load_data/Monthly-hourly-load-values_2006-2015.xlsx"), skip = 3) %>% 
+full_df <- read_xlsx(here("data/raw/ENTSOE/ENTSOE_load_data/Monthly-hourly-load-values_2006-2015.xlsx"), skip = 3) %>% 
   pivot_longer(!c(Country, Year, Month, Day, `Coverage ratio`), names_to = "Hour") %>% 
   rename(cov_ratio = `Coverage ratio`, 
          country_code = Country) %>% 
